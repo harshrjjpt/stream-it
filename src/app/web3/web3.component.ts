a6ef54c769bde460538a8a04cb8ab9429e92aed6
harsh <harshrjjpt@gmail.com>
2022-07-19 22:13:42 +0530

    Added network change logic in metamask

--- src/app/web3/web3.component.ts
+++ src/app/web3/web3.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { exhaustAll } from 'rxjs';
 import Web3 from 'web3';
 
 
@@ -11,12 +12,17 @@ declare let window: any;
 })
 export class Web3Component {
 
+
+
   web3:any = null;
   check:any;
   hello:boolean = window.ethereum;
   account:any = null;
   mybalance:any;
   result:any;
+  error:any;
+
+   
 
 
   constructor() {
@@ -28,6 +34,64 @@ export class Web3Component {
   }
 
 
+  networks:any = {
+    polygon: {
+      chainId: `0x${Number(137).toString(16)}`,
+      chainName: "Polygon Mainnet",
+      nativeCurrency: {
+        name: "MATIC",
+        symbol: "MATIC",
+        decimals: 18
+      },
+      rpcUrls: ["https://polygon-rpc.com/"],
+      blockExplorerUrls: ["https://polygonscan.com/"]
+    },
+    bsc: {
+      chainId: `0x${Number(56).toString(16)}`,
+      chainName: "Binance Smart Chain Mainnet",
+      nativeCurrency: {
+        name: "Binance Chain Native Token",
+        symbol: "BNB",
+        decimals: 18
+      },
+      rpcUrls: [
+        "https://bsc-dataseed1.binance.org",
+        "https://bsc-dataseed2.binance.org",
+        "https://bsc-dataseed3.binance.org",
+        "https://bsc-dataseed4.binance.org",
+        "https://bsc-dataseed1.defibit.io",
+        "https://bsc-dataseed2.defibit.io",
+        "https://bsc-dataseed3.defibit.io",
+        "https://bsc-dataseed4.defibit.io",
+        "https://bsc-dataseed1.ninicoin.io",
+        "https://bsc-dataseed2.ninicoin.io",
+        "https://bsc-dataseed3.ninicoin.io",
+        "https://bsc-dataseed4.ninicoin.io",
+        "wss://bsc-ws-node.nariox.org"
+      ],
+      blockExplorerUrls: ["https://bscscan.com"]
+    },
+    celo: {
+      chainId: `0x${Number(42220).toString(16)}`,
+      chainName: "Celo Mainnet",
+      nativeCurrency: {
+        name: "Celo Mainnet",
+        symbol: "CELO",
+        decimals: 18
+      },
+      rpcUrls: [
+        "https://forno.celo.org"
+      ],
+      blockExplorerUrls: ["https://explorer.celo.org"]
+    }
+  };
+  
+
+
+
+
+ ///***********LOGIN LOGIC*************//// 
+
   async  loginWithMetamask() {
     const accounts = await window.ethereum.request({method: 'eth_requestAccounts'}).catch((err:any)=> {
       console.log(err.code);
@@ -35,6 +99,12 @@ export class Web3Component {
     this.account = accounts[0];
     console.log(this.account);
   }
+
+
+
+
+
+ ///***********CHECKING BALANCE LOGIC*************//// 
 
   async checkBalance () {
       this.mybalance = await window.ethereum.request({method: "eth_getBalance",
@@ -50,21 +120,68 @@ export class Web3Component {
      console.log(this.mybalance);
   }
 money:any = 100000000000000000;
+money2:any = 200000000000000000;
+
+
+ ///***********MAKING TRANSACTION LOGIC*************//// 
+
+
+
   async sendpayment() {
     let  params = [{
-      once: '0x00', // ignored by MetaMask
-  gasPrice: Number(21000).toString(16), // customizable by user during MetaMask confirmation.
-  gas: Number(2500000).toString(16), // customizable by user during MetaMask confirmation.
   to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
   from: this.account, // must match user's active address.
   value: Number(this.money).toString(16), // Only required to send ether to the recipient from the initiating external account.
-  data:
-  Number(this.money).toString(16), // Optional, but used for defining smart contract creation and interaction.
-  chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
     }]
     this.result = window.ethereum.request({method: "eth_sendTransaction", params}).catch((err:any)=> {
       console.log(err);
     })
   }
+
+  async sendpayment2() {
+    let  params = [{
+  to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
+  from: this.account, // must match user's active address.
+  value: Number(this.money2).toString(16), // Only required to send ether to the recipient from the initiating external account.
+    }]
+    this.result = window.ethereum.request({method: "eth_sendTransaction", params}).catch((err:any)=> {
+      console.log(err);
+      this.error = err.message;
+
+    })
+  }
+
+
+
+ ///***********CHANGING NETWORK LOGIC*************//// 
+
+ networkName = "polygon";
+
+
+ async changeNetwork  (networkName:any) {
+  try {
+    if (!window.ethereum) throw new Error("No crypto wallet found");
+    await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          ...this.networks[networkName]
+        }
+      ]
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// async handleNetworkSwitch (networkName:any) => {
+//   await changeNetwork({ networkName });
+// };
+
+
+
+
+
+
 
 }
